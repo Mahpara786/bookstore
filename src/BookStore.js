5fccd35a20314270200335a93fbca20c8c19dd1a
Mahpara786 <mahpara8604@gmail.com>
2021-08-10 13:30:47 +0530

    booldetailsWithDetails

--- src/BookStore.js
+++ src/BookStore.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
 function BookStore() {
     return (
-        <section>
+        <section className = "booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
             <Book />
         </section>
     )
@@ -14,11 +19,15 @@ const Book = () => {
     <Img></Img>
     <Title/>
     <Author/>
+    <Price/>
+    <Quality/>
     </article>
 }
 const Img = () => <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbP9KQQ7BsbbhLVjofmvQ3vZS3Ge7YHaNh2OB2blVzT-kfdkIWK1_D3KqPri0&usqp=CAc" alt=""></img>
 
 const Title = () => <h1>Great Stories to read</h1>
  const Author = () => <h3>Dr Joseph Murthy</h3>
+ const Price = () => <h4>Rs.125</h4>
+ const Quality = () => <h5>Hardcover</h5>
 
 export default BookStore
