77fd9ea7f9aaa174bb5fdbb3cdd3023d4ff3101d
Mahpara786 <mahpara8604@gmail.com>
2021-08-10 11:05:17 +0530

    firstComponent

--- src/firstComponent.js
+++ src/firstComponent.js
@@ -6,6 +6,7 @@ function FirstComponent() {
         <React.Fragment>
             <div>
                 <h2>This is my first component</h2>
+                <img src="penguin.jpg" alt="the penguin" width="500" height="320"></img>
             </div>
         </React.Fragment>
     )
