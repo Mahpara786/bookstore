3a4c74844ca2ae1e1d0f902be1bdab92c7de696d
Mahpara786 <mahpara8604@gmail.com>
2021-08-10 11:09:38 +0530

    secondComponent

---
+++ src/SecondComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {react} from "react-dom";
+
+
+function SecondComponent() {
+    return (
+        <React.Fragment>
+            <div>
+                <h2>This is my Second component</h2>
+                <h3>Name Mahpara</h3>
+                <h4>Emp id 701566</h4>
+            </div>
+        </React.Fragment>
+    )
+}
+export default SecondComponent
