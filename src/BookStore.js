441e71faf2a3ca22f37be65ef5779053a53e510f
Mahpara786 <mahpara8604@gmail.com>
2021-08-10 14:57:01 +0530

    styleinsidejs

--- src/BookStore.js
+++ src/BookStore.js
@@ -25,8 +25,8 @@ const Book = () => {
 }
 const Img = () => <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbP9KQQ7BsbbhLVjofmvQ3vZS3Ge7YHaNh2OB2blVzT-kfdkIWK1_D3KqPri0&usqp=CAc" alt=""></img>
 
-const Title = () => <h1>Great Stories to read</h1>
- const Author = () => <h3>Dr Joseph Murthy</h3>
+const Title = () => <h1 style={{color:'#617d98', fontSize:'0.75rem', margin:"0.25rem"}}>Great Stories to read</h1>
+ const Author = () => <h3 style={{color:'Highlight', fontSize:'0.75rem', margin:"0.25rem"}}>Dr Joseph Murthy</h3>
  const Price = () => <h4>Rs.125</h4>
  const Quality = () => <h5>Hardcover</h5>
 
