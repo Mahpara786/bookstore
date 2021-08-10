72f3627e80fe453afaa940a7c5ddd4d522ccbe9c
Mahpara786 <mahpara8604@gmail.com>
2021-08-10 15:53:37 +0530

    With props

--- src/BookStore.js
+++ src/BookStore.js
@@ -2,32 +2,41 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
-function BookStore() {
+const FirstBook ={
+         title : " Great Stories to read " ,
+         author : " Dr Joseph Murthy " ,
+         img : " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGgureMg79tqeB3mgwK02Kawx8_Bg3SzOsS_qTwuGTTJT0Ygk-V7Oh0N5nl08&usqp=CAc ",
+
+    }
+    const SecondBook ={
+        title : "Because life is a gift " ,
+        author : " Disha " ,
+        img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNLvc_7RJDyxEv0F_9ZbVAPW0P7RUvT0ac8nbM3ZieZw7cRbZgaFYIJkq1OQU8u7tB6jSsO1U&usqp=CAc",
+
+   }
+   const ThirdBook ={
+    title : " Fear not be strong " ,
+    author : " Swami Vivekanand " ,
+    img : " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7UMloPLjPfclANwuOYIpwFhdsdsiQw7Y42XYklzdJSWdyFHLqotKiSnKjRQ&usqp=CAc ",
+
+}
+    function BookStore() {
     return (
         <section className = "booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book img={FirstBook.img} title={FirstBook.title} author={FirstBook.author} />
+            <Book img={SecondBook.img} title={SecondBook.title} author={SecondBook.author} />
+            <Book img={ThirdBook.img} title={ThirdBook.title} author={ThirdBook.author} />
         </section>
     )
 }
 
-const Book = () => {
-    return <article>
-    <Img></Img>
-    <Title/>
-    <Author/>
-    <Price/>
-    <Quality/>
-    </article>
+const Book = (props) => {
+    return (<article className='book'>
+        <img src={props.img}></img>
+        <h1>{props.title}</h1>
+        <h3>{props.author}</h3>
+         </article>
+    );
 }
-const Img = () => <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbP9KQQ7BsbbhLVjofmvQ3vZS3Ge7YHaNh2OB2blVzT-kfdkIWK1_D3KqPri0&usqp=CAc" alt=""></img>
-
-const Title = () => <h1 style={{color:'#617d98', fontSize:'0.75rem', margin:"0.25rem"}}>Great Stories to read</h1>
- const Author = () => <h3 style={{color:'Highlight', fontSize:'0.75rem', margin:"0.25rem"}}>Dr Joseph Murthy</h3>
- const Price = () => <h4>Rs.125</h4>
- const Quality = () => <h5>Hardcover</h5>
 
 export default BookStore
